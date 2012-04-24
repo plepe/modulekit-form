308b03f1f7f943cbc3666009a6b02c7bd6d93d15
Stephan Plepelits <skunk@xover.mud.at>
2012-04-24 07:10:05 +0200

    Doc: Bugfix replacing examples

--- doc/index.php
+++ doc/index.php
@@ -46,11 +46,13 @@ if(preg_match_all("/Example: (\{.*\})/", $text, $m, PREG_OFFSET_CAPTURE)) {
   $new_text="";
   $last=0;
   foreach($m[1] as $i=>$m1) {
-    $new_text.=substr($text, $last, $m1[1]);
+    $new_text.=substr($text, $last, $m1[1]-$last);
     $url=substr($text, $m1[1], strlen($m1[0]));
     $new_text.="<a href='../try.php?q=".urlencode(html_entity_decode($url))."'>$url</a>";
     $last=$m1[1]+strlen($m1[0]);
   }
+  $new_text.=substr($text, $last);
+
   $text=$new_text;
 }
 
