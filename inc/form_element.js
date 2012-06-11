a9fc843605c700ba7c2a35f018c234f70c5473fe
Stephan Plepelits <skunk@xover.mud.at>
2012-06-11 17:54:26 +0200

    Bugfix: Don't show ':' after form element description

--- inc/form_element.js
+++ inc/form_element.js
@@ -74,7 +74,7 @@ form_element.prototype.show=function() {
     if(this.def.desc) {
       var div=document.createElement("div");
       div.className="form_desc";
-      var text=document.createTextNode(this.def.desc+":");
+      var text=document.createTextNode(this.def.desc);
       div.appendChild(text);
       td.appendChild(div);
     }
