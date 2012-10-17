31e5ad122a4198adebb4de0da06c45daa0928ca3
Stephan Plepelits <skunk@xover.mud.at>
2012-10-17 07:55:28 +0200

    html_export_var(): Avoid name-clash

--- inc/functions.php
+++ inc/functions.php
@@ -1,10 +1,10 @@
 <?
-function html_export_var($data) {
-  print "<script type='text/javascript'>\n";
-  foreach($data as $k=>$v) {
-    print "var $k=".json_encode($v).";\n";
+if(!function_exists("html_export_var")) {
+  function html_export_var($data) {
+    print "<script type='text/javascript'>\n";
+    foreach($data as $k=>$v) {
+      print "var $k=".json_encode($v).";\n";
+    }
+    print "</script>\n";
   }
-  print "</script>\n";
 }
-
-
