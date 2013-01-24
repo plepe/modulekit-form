ee9d6c7840c98fabe7dbd3d890854143795b4909
Stephan Plepelits <skunk@cg.tuwien.ac.at>
2013-01-24 10:11:32 +0100

    Form Compat: Module to simulate old form interface

---
+++ modules/form_compat/form_compat.php
@@ -0,0 +1,56 @@
+<?
+// Compatibility layer to the branch-1.0 version of the form library
+global $form_compat;
+$form_compat=array();
+
+function form_compat_get_form($varname, $def) {
+  global $form_compat;
+
+  if($varname===null) {
+    foreach($_REQUEST as $k=>$v) {
+      if(preg_match("/^data_?/", $k))
+	$varname=$k;
+    }
+  }
+
+  foreach($form_compat as $f) {
+    if($f['def']==$def) {
+      print "FOUND\n";
+      return $f['form'];
+    }
+  }
+
+  $f=array();
+  $f['varname']=$varname;
+  $f['def']=$def;
+  $f['form']=new form($varname, $def);
+
+  $form_compat[]=$f;
+
+  return $f['form'];
+}
+
+function form_show($def, $data, $varname, $moreparam=array()) {
+   $form=form_compat_get_form($varname, $def); 
+
+   $form->set_data($data);
+   return $form->show();
+}
+
+function form_get_data($def, $data) {
+   $form=form_compat_get_form(null, $def); 
+
+   return $form->get_data();
+}
+
+function form_save_data($def, $data) {
+   $form=form_compat_get_form(null, $def); 
+
+   return $form->save_data();
+}
+
+function form_check($def, $data) {
+   $form=form_compat_get_form(null, $def); 
+
+   return $form->errors();
+}
