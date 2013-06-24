c096f6d56fda6e76a5fb84557abc18cd326f428b
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2013-06-24 14:21:54 +0200

    Form Element File: Fix JS functions (esp. connect)

--- inc/form_element_file.js
+++ inc/form_element_file.js
@@ -1,4 +1,3 @@
-/*
 form_element_file.inherits_from(form_element);
 function form_element_file() {
 }
@@ -7,44 +6,10 @@ form_element_file.prototype.init=function(id, def, options, form_parent) {
   this.parent("form_element_file").init.call(this, id, def, options, form_parent);
 }
 
-form_element_file.prototype.connect=function(dom_parent) {
-  this.parent("form_element_file").connect.call(this, dom_parent);
-  this.dom_element=this.dom_parent.getElementsByTagName("input")[0];
-
-  this.dom_element.onchange=this.notify_change.bind(this);
-}
-
-form_element_file.prototype.show_element=function() {
-  var div=this.parent("form_element_file").show_element.call(this);
-
-  var cls="form_orig";
-  if(this.is_modified())
-    cls="form_modified";
-
-  var input=document.createElement("input");
-  input.type="text";
-  input.className=cls;
-  input.name=this.options.var_name;
-  if(this.data)
-    input.value=this.data;
-  div.appendChild(input);
-  this.dom_element=input;
-  this.dom_element.onchange=this.notify_change.bind(this);
-
-  return div;
-}
-
-form_element_file.prototype.set_data=function(data) {
-  this.parent("form_element_file").set_data.call(this, data);
-
-  if(this.dom_element)
-    this.dom_element.value=this.data;
-}
-
 form_element_file.prototype.refresh=function() {
   var cls;
 
-  this.parent("form_element_file").check_modified.call(this);
+  //this.parent("form_element_file").check_modified.call(this);
 
   if(!this.dom_element)
     return;
@@ -55,5 +20,18 @@ form_element_file.prototype.refresh=function() {
     cls="form_orig";
 
   this.dom_element.className=cls;
+  this.dom_element.onchange=this.notify_change.bind(this);
 }
-*/
+
+form_element_file.prototype.connect=function(dom_parent) {
+  this.parent("form_element_file").connect.call(this, dom_parent);
+
+  var obs=this.dom_parent.getElementsByTagName("input");
+  for(var i=0; i<obs.length; i++) {
+    if(obs[i].name==this.options.var_name+"[file]")
+      this.dom_element=obs[i];
+  }
+
+  if(this.dom_element)
+    this.dom_element.onchange=this.notify_change.bind(this);
+}
