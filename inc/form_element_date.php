8bf00317378e5dcc702da9c716e664054670126d
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2013-07-13 07:39:51 +0200

    Form Element Date: Don't change format internally

--- inc/form_element_date.php
+++ inc/form_element_date.php
@@ -34,13 +34,18 @@ class form_element_date extends form_element_text {
     return $form_element_date_display_format[$this->type()];
   }
 
-  function set_request_data($data) {
+  function get_data() {
     global $form_element_date_value_format;
 
-    $dt=DateTime::createFromFormat($this->date_format(), $data);
-    $data=$dt->format($form_element_date_value_format[$this->type()]);
+    // datetime can be stored internally either in value or display format
+    $dt=DateTime::createFromFormat($this->date_format(), $this->data);
+    if(!$dt) {
+      $dt=DateTime::createFromFormat($form_element_date_value_format[$this->type()], $this->data);
+      if(!$dt)
+	return null;
+    }
 
-    parent::set_request_data($data);
+    return $dt->format($form_element_date_value_format[$this->type()]);
   }
 
   function show_element($document) {
@@ -49,8 +54,12 @@ class form_element_date extends form_element_text {
     $format=$this->date_format();
 
     if($this->data!==null) {
-      $dt=new DateTime($this->data);
-      $this->dom_element->setAttribute("value", $dt->format($format));
+      if($this->get_data()!==null) {
+	$dt=new DateTime($this->get_data());
+	$this->dom_element->setAttribute("value", $dt->format($format));
+      }
+      else
+	$this->dom_element->setAttribute("value", $this->data);
     }
 
     return $div;
