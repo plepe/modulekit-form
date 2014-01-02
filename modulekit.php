169fb1bfed4d178b25ffb886b4c5cc90551a1230
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2014-01-02 13:30:55 +0100

    Form Element Select: show descriptions in smaller italic letters

--- modulekit.php
+++ modulekit.php
@@ -61,6 +61,7 @@ $include_css=array(
   "inc/form_element_form.css",
   "inc/form_element_keywords.css",
   "inc/form_element_textarea.css",
+  "inc/form_element_select.css",
 
   "inc/theme.css",
 );
