92ae134d05c675a8fce7ae7fb6d24182053c9cac
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-06-10 11:55:28 +0200

    Editor: fix headers

--- editor.php
+++ editor.php
@@ -1,12 +1,19 @@
 <?php
 Header("content-type: text/html; charset=utf-8");
 include "modulekit/loader.php"; /* loads all php-includes */
+call_hooks("init");
 
 ?>
+<!DOCTYPE HTML>
 <html>
 <head>
 <?php print modulekit_include_js(); /* prints all js-includes */ ?>
 <?php print modulekit_include_css(); /* prints all css-includes */ ?>
+<?php print_add_html_headers(); ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel='stylesheet' type='text/css' href='demo.css'/>
+<script type='text/javascript' src='demo_combined.js'></script>
+<script type='text/javascript' src='demo_functions.js'></script>
 </head>
 <body>
 <?php
