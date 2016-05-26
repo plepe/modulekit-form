4420dad8df3639c46dd4e8803a11c6e63fead777
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-05-26 14:55:21 +0200

    Tests: infrastructure for JS-only tests

--- create_jstest.php
+++ create_jstest.php
@@ -5,8 +5,6 @@ call_hooks("init");
 
 $test = $argv[1];
 
-include "test/combined/{$test}.php";
-
 $f = fopen("test.html", "w");
 fputs($f, "<!DOCTYPE HTML>\n");
 fputs($f, "<html>\n");
@@ -16,6 +14,7 @@ fputs($f, "<link rel='stylesheet' href='./node_modules/mocha/mocha.css'>\n");
 fputs($f, "<div id='mocha'></div>\n");
 fputs($f, "<script src='./node_modules/mocha/mocha.js'></script>\n");
 fputs($f, "<script src='./node_modules/chai/chai.js'></script>\n");
+fputs($f, "<script src='./node_modules/jquery/dist/jquery.min.js'></script>\n");
 fputs($f, "<script>\n");
 fputs($f, "mocha.ui('bdd'); \n");
 fputs($f, "mocha.reporter('html');\n");
@@ -28,11 +27,10 @@ fputs($f, get_add_html_headers());
 fputs($f, '<meta name="viewport" content="width=device-width, initial-scale=1">' . "\n");
 fputs($f, "</head>\n");
 fputs($f, "<body>\n");
-fputs($f, "<form method='post'>\n");
-fputs($f, $form->show());
+fputs($f, "<form id='test_form'>\n");
 fputs($f, "<input type='submit'>\n");
 fputs($f, "</form>\n");
-fputs($f, "<script src='test/combined/{$test}.js'></script>\n");
+fputs($f, "<script src='test/js/{$test}.js'></script>\n");
 fputs($f, "<script>\n");
 fputs($f, "call_hooks('init');\n");
 fputs($f, "if (window.mochaPhantomJS) { mochaPhantomJS.run(); }\n");
