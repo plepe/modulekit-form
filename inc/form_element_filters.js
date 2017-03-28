b27279870aaeed003f84485a86e3818e9592b7a4
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-03-28 12:10:12 +0200

    Form Element Filters: create as copy of form element array

---
+++ inc/form_element_filters.js
@@ -0,0 +1,469 @@
+form_element_filters.inherits_from(form_element);
+function form_element_filters() {
+}
+
+form_element_filters.prototype.init=function(id, def, options, form_parent) {
+  this.parent("form_element_filters").init.call(this, id, def, options, form_parent);
+
+  this.build_form();
+}
+
+form_element_filters.prototype.build_form=function() {
+  this.elements={};
+  this.element_divs = {};
+
+  if(!this.data) {
+    this.data={};
+    for(var k=0; k<this.def['default']; k++) {
+      this.data[k]=null;
+    }
+  }
+
+  for(var k in this.data) {
+    this.create_element(k);
+  }
+}
+
+form_element_filters.prototype.index_element=function(el) {
+  var i=1;
+  for(var k in this.elements) {
+    if(el==k)
+      return "#"+i;
+    i++;
+  }
+
+  return null;
+}
+
+form_element_filters.prototype.create_element=function(k) {
+  var element_def=new clone(this.def.def);
+  var element_class=get_form_element_class(element_def);
+  var element_id=this.id+"_"+k;
+  var element_options=new clone(this.options);
+  element_options.var_name=element_options.var_name+"["+k+"]";
+  element_def._name=function(k) {
+    return this.index_element(k);
+  }.bind(this, k);
+
+  if(class_exists(element_class)) {
+    this.elements[k]=eval("new "+element_class+"()");
+    this.elements[k].init(element_id, element_def, element_options, this);
+  }
+}
+
+form_element_filters.prototype.connect=function(dom_parent) {
+  this.parent("form_element_filters").connect.call(this, dom_parent);
+
+  var current=this.dom_parent.firstChild;
+  this.elements={};
+  this.element_divs = {};
+  while(current) {
+    var k;
+    if((current.nodeName=="DIV")&&(k=current.getAttribute("form_element_num"))) {
+      this.element_divs[k] = current;
+      var element_class="form_element_"+this.def.def.type;
+      var element_id=this.id+"_"+k;
+      var element_options=new clone(this.options);
+      element_options.var_name=element_options.var_name+"["+k+"]";
+      var element_def=new clone(this.def.def);
+      element_def._name=function(k) {
+	return this.index_element(k);
+      }.bind(this, k);
+
+      if(class_exists(element_class)) {
+	this.elements[k]=eval("new "+element_class+"()");
+	this.elements[k].init(element_id, element_def, element_options, this);
+	this.elements[k].connect(current.firstChild.firstChild);
+      }
+
+      // modify part actions
+      var input=current.firstChild;
+      while((input)&&(input.className!="form_element_filters_part_element_actions")) input=input.nextSibling;
+      if(!input)
+	break;
+      input=input.firstChild;
+      while(input) {
+	if(input.name==this.options.var_name+"[__remove]["+k+"]") {
+	  input.onclick=this.remove_element.bind(this, k);
+	}
+	else if(input.name==this.options.var_name+"[__order_up]["+k+"]") {
+	  input.onclick=this.order_up.bind(this, k);
+	}
+	else if(input.name==this.options.var_name+"[__order_down]["+k+"]") {
+	  input.onclick=this.order_down.bind(this, k);
+	}
+	input=input.nextSibling;
+      }
+    }
+
+    // modify actions
+    if(current.className=="form_element_filters_actions") {
+      input=current.firstChild;
+      while(input) {
+	if(input.name==this.options.var_name+"[__new]") {
+	  this.action_add = input;
+	  this.action_add.onclick = this.add_element.bind(this);
+	}
+	input=input.nextSibling;
+      }
+    }
+
+    current=current.nextSibling;
+  }
+}
+
+form_element_filters.prototype.finish_connect=function() {
+  this.parent("form_element_filters").finish_connect.call(this);
+
+  for(var k in this.elements) {
+    this.elements[k].finish_connect();
+  }
+}
+
+form_element_filters.prototype.get_data=function() {
+  var ret={};
+  var count=0;
+
+  for(var i in this.elements) {
+    var d = this.elements[i].get_data();
+
+    if(this.def.exclude_null_values && (d === null))
+      continue;
+
+    if(this.elements[i].include_data()) {
+      ret[i] = d;
+      count++;
+    }
+  }
+
+  if(count==0)
+    return this.def.empty_value || null;
+
+  return ret;
+}
+
+form_element_filters.prototype.set_data=function(data) {
+  this.data=data;
+  this.build_form();
+
+  if(data)
+    for(var k in data) {
+      if(typeof this.elements[k]!="undefined")
+	this.elements[k].set_data(data[k]);
+    }
+
+  if(this.dom) {
+    var old_dom = this.dom;
+    var par = this.dom.parentNode;
+    var div = this.show_element();
+
+    par.insertBefore(div, old_dom);
+    par.removeChild(old_dom);
+  }
+
+  this.data=null;
+}
+
+form_element_filters.prototype.set_orig_data=function(data) {
+  for(var k in this.elements) {
+    if(!data)
+      this.elements[k].set_orig_data(null);
+    else if(k in data)
+      this.elements[k].set_orig_data(data[k]);
+    else
+      this.elements[k].set_orig_data(null);
+  }
+}
+
+form_element_filters.prototype.get_orig_data=function() {
+  var ret={};
+
+  for(var i in this.elements) {
+    ret[i]=this.elements[i].get_orig_data();
+  }
+
+  return ret;
+}
+
+form_element_filters.prototype.show_element_part=function(k, element) {
+  var order;
+  var removeable;
+  if(!('order' in this.def) || this.def.order)
+    order = 'order'
+  else
+    order = 'no_order';
+  if(!('removeable' in this.def) || this.def.removeable !== false)
+    removeable = 'removeable'
+  else
+    removeable = 'not_removeable';
+
+  // wrapper #k
+  var div=document.createElement("div");
+  div.setAttribute("form_element_num", k);
+  div.className="form_element_filters_part";
+
+  // element #k
+  var el_div=document.createElement("span");
+  el_div.setAttribute("form_element_num", k);
+  el_div.className="form_element_filters_part_element form_element_"+element.type() + " form_element_filters_" + order + "_" + removeable;
+  div.appendChild(el_div);
+
+  el_div.appendChild(element.show_element());
+
+  // errors #k
+  el_div.appendChild(element.show_div_errors());
+
+  // Actions #k
+  var el_div=document.createElement("span");
+  el_div.setAttribute("form_element_num", k);
+  el_div.className="form_element_filters_part_element_actions";
+  div.appendChild(el_div);
+
+  if(order == 'order') {
+    var input=document.createElement("input");
+    input.type="submit";
+    input.name=this.options.var_name+"[__order_up]["+k+"]";
+    input.value="↑";
+    input.onclick=this.order_up.bind(this, k);
+    el_div.appendChild(input);
+
+    var input=document.createElement("input");
+    input.type="submit";
+    input.name=this.options.var_name+"[__order_down]["+k+"]";
+    input.value="↓";
+    input.onclick=this.order_down.bind(this, k);
+    el_div.appendChild(input);
+  }
+
+  if (removeable === 'removeable') {
+    var input=document.createElement("input");
+    input.type="submit";
+    input.name=this.options.var_name+"[__remove]["+k+"]";
+    input.value="X";
+    input.onclick=this.remove_element.bind(this, k);
+    el_div.appendChild(input);
+  }
+
+  return div;
+}
+
+form_element_filters.prototype.show_element=function() {
+  var div=this.parent("form_element_filters").show_element.call(this);
+  this.get_data();
+
+  for(var k in this.elements) {
+    var part_div=this.show_element_part(k, this.elements[k]);
+    this.element_divs[k] = part_div;
+    div.appendChild(part_div);
+  }
+
+  var el_div=document.createElement("div");
+  el_div.className="form_element_filters_actions";
+  div.appendChild(el_div);
+
+  this.action_add=document.createElement("input");
+  this.action_add.type="submit";
+  this.action_add.name=this.options.var_name+"[__new]";
+  if("button:add_element" in this.def) {
+    if(typeof(this.def['button:add_element']) == "object")
+      this.action_add.value = lang(this.def['button:add_element']);
+    else
+      this.action_add.value = this.def['button:add_element'];
+  }
+  else
+    this.action_add.value=lang('form:add_element');
+  this.action_add.onclick=this.add_element.bind(this);
+  el_div.appendChild(this.action_add);
+
+  return div;
+}
+
+form_element_filters.prototype.all_errors=function(list) {
+  this.parent("form_element_filters").all_errors.call(this, list);
+
+  for(var k in this.elements)
+    this.elements[k].all_errors(list);
+}
+
+form_element_filters.prototype.errors=function(list) {
+  this.parent("form_element_filters").errors.call(this, list);
+
+  var count = 0;
+  for(var k in this.elements)
+    count++;
+
+  if('max' in this.def && (count > this.def.max))
+    list.push(lang('form_element_filters:error_max', 0, this.def.max));
+
+  if('min' in this.def && (count < this.def.min))
+    list.push(lang('form_element_filters:error_min', 0, this.def.min));
+}
+
+form_element_filters.prototype.is_complete=function() {
+// TODO?
+//  if(this.changed_count)
+//    return false;
+
+  for(var i in this.elements) {
+    if(!this.elements[i].is_complete())
+      return false;
+  }
+
+  return true;
+}
+
+form_element_filters.prototype.add_element=function() {
+  var highest_id=0;
+
+  for(var i in this.elements) {
+    i = parseInt(i);
+    if(i>highest_id)
+      highest_id=i;
+  }
+
+  highest_id=parseInt(highest_id)+1;
+  this.create_element(highest_id);
+
+  var current=document.getElementById(this.id).firstChild;
+  while(current) {
+    if(current.className=="form_element_filters_actions") {
+      current.parentNode.insertBefore(this.show_element_part(highest_id, this.elements[highest_id]), current);
+      break;
+    }
+    current=current.nextSibling;
+  }
+
+  this.form_root.form.resize();
+
+  return false;
+}
+
+form_element_filters.prototype.remove_element=function(k) {
+  var current=document.getElementById(this.id).firstChild;
+  while(current) {
+    if((current.className=="form_element_filters_part")&&(current.getAttribute("form_element_num")==k)) {
+      current.parentNode.removeChild(current);
+      delete(this.elements[k]);
+      break;
+    }
+
+    current=current.nextSibling;
+  }
+
+  this.show_errors();
+  this.form_root.form.resize();
+
+  return false;
+}
+
+form_element_filters.prototype.order_up=function(k) {
+  var current=document.getElementById(this.id).firstChild;
+  var prev=null;
+
+  while(current) {
+    if(current.className=="form_element_filters_part") {
+      if(current.getAttribute("form_element_num")==k) {
+	if(prev)
+	  current.parentNode.insertBefore(current, prev);
+	break;
+      }
+
+      prev=current;
+    }
+
+    current=current.nextSibling;
+  }
+
+  this.show_errors();
+  this.form_root.form.resize();
+
+  return false;
+}
+
+form_element_filters.prototype.order_down=function(k) {
+  var current=document.getElementById(this.id).lastChild;
+  var next=null;
+
+  while(current) {
+    if(current.className=="form_element_filters_part") {
+      if(current.getAttribute("form_element_num")==k) {
+	if(next)
+	  current.parentNode.insertBefore(next, current);
+	break;
+      }
+
+      next=current;
+    }
+
+    current=current.previousSibling;
+  }
+
+  this.show_errors();
+  this.form_root.form.resize();
+
+  return false;
+}
+
+form_element_filters.prototype.refresh=function(force) {
+  this.parent("form_element_filters").refresh.call(this, force);
+
+  for(var i in this.elements)
+    this.elements[i].refresh(force);
+
+  var count = 0;
+  for(var k in this.elements)
+    count++;
+
+  if('max' in this.def && (count >= this.def.max))
+    this.action_add.className = "reached_max";
+  else
+    this.action_add.className = "";
+}
+
+form_element_filters.prototype.is_modified=function() {
+  var orig_data=this.get_orig_data();
+
+  for(var i in this.elements) {
+    // new key, was not present in orig_data
+    if(typeof orig_data[i]=="undefined")
+      return true;
+
+    // data of sub-elements were changed
+    if(this.elements[i].is_modified())
+      return true;
+  }
+
+  for(var i in orig_data) {
+    // key has been removed
+    if(typeof this.elements[i]=="undefined")
+      return true;
+  }
+
+  return false;
+}
+
+form_element_filters.prototype.show_errors=function() {
+  this.parent("form_element_filters").show_errors.call(this);
+
+  for(var i in this.elements)
+    this.elements[i].show_errors();
+}
+
+form_element_filters.prototype.notify_child_change=function(element) {
+  // show errors of current element
+  this.parent("form_element_filters").show_errors.call(this);
+
+  this.parent("form_element_filters").notify_child_change.call(this, element);
+}
+
+form_element_filters.prototype.check_required=function(list, param) {
+  if(this.required()) {
+    for(var k in this.elements)
+      return;
+
+    if(param.length<1)
+      list.push(lang('form:require_value'));
+    else
+      list.push(param[0]);
+  }
+}
---
+++ inc/form_element_filters.php
@@ -0,0 +1,352 @@
+<?php
+class form_element_filters extends form_element {
+  public $changed_count;
+
+  function __construct($id, $def, $options, $form_parent) {
+    parent::__construct($id, $def, $options, $form_parent);
+    $this->changed_count=false;
+
+    $this->build_form();
+  }
+
+  function is_complete() {
+    if($this->changed_count)
+      return false;
+
+    foreach($this->elements as $k=>$element) {
+      if(!$element->is_complete())
+	return false;
+    }
+
+    return true;
+  }
+
+  function all_errors(&$errors) {
+    parent::all_errors($errors);
+
+    foreach($this->elements as $k=>$element) {
+      $element->all_errors($errors);
+    }
+  }
+
+  function errors(&$errors) {
+    parent::errors($errors);
+
+    if(isset($this->def['min']) && (sizeof($this->elements) < $this->def['min'])) {
+      $errors[] = lang('form_element_filters:error_min', 0, $this->def['min']);
+    }
+
+    if(isset($this->def['max']) && (sizeof($this->elements) > $this->def['max'])) {
+      $errors[] = lang('form_element_filters:error_max', 0, $this->def['max']);
+    }
+  }
+
+  function get_data() {
+    $data=array();
+    foreach($this->elements as $k=>$element) {
+      $d = $element->get_data();
+
+      if((isset($this->def['exclude_null_values']) && ($this->def['exclude_null_values'])) && ($d === null))
+	continue;
+
+      if($element->include_data())
+	$data[$k] = $d;
+    }
+
+    if(!sizeof($data))
+      return array_key_exists('empty_value', $this->def) ?
+        $this->def['empty_value'] : null;
+
+    return $data;
+  }
+
+  function set_data($data) {
+    $this->data=$data;
+    $this->build_form();
+
+    if($data&&is_array($data))
+      foreach($data as $k=>$d) {
+	if(isset($this->elements[$k]))
+	  $this->elements[$k]->set_data($d);
+      }
+
+    unset($this->data);
+  }
+
+  function set_request_data($data) {
+    $ret = true;
+
+    // request data empty? -> empty array
+    if($data === null) {
+      $data = array();
+    }
+
+    $this->data=$data;
+    if(isset($this->data['__new'])) {
+      unset($this->data['__new']);
+      $this->data[]=null;
+      $this->changed_count=true;
+      $ret = false;
+    }
+    if(isset($this->data['__remove'])) {
+      $remove=$this->data['__remove'];
+      unset($this->data['__remove']);
+      $ret = false;
+    }
+    if(isset($this->data['__order_up'])) {
+      $order_up=$this->data['__order_up'];
+      unset($this->data['__order_up']);
+      $ret = false;
+    }
+    if(isset($this->data['__order_down'])) {
+      $order_down=$this->data['__order_down'];
+      unset($this->data['__order_down']);
+      $ret = false;
+    }
+
+    $this->build_form();
+
+    if(isset($order_up)) {
+      foreach($order_up as $i=>$dummy) {
+	$keys=array_keys($this->data);
+	$elements=array_values($this->elements);
+	$pos=array_search($i, $keys);
+
+	$keys=array_merge(
+	  array_slice($keys, 0, $pos-1),
+	  array_slice($keys, $pos, 1),
+	  array_slice($keys, $pos-1, 1),
+	  array_slice($keys, $pos+1)
+	);
+	$elements=array_merge(
+	  array_slice($elements, 0     , $pos-1),
+	  array_slice($elements, $pos  , 1),
+	  array_slice($elements, $pos-1, 1),
+	  array_slice($elements, $pos+1)
+	);
+
+	$this->elements=array_combine($keys, $elements);
+      }
+    }
+
+    if(isset($order_down)) {
+      foreach($order_down as $i=>$dummy) {
+	$keys=array_keys($this->data);
+	$elements=array_values($this->elements);
+	$pos=array_search($i, $keys);
+
+	$keys=array_merge(
+	  array_slice($keys, 0     , $pos),
+	  array_slice($keys, $pos+1, 1),
+	  array_slice($keys, $pos  , 1),
+	  array_slice($keys, $pos+2)
+	);
+	$elements=array_merge(
+	  array_slice($elements, 0     , $pos),
+	  array_slice($elements, $pos+1, 1),
+	  array_slice($elements, $pos  , 1),
+	  array_slice($elements, $pos+2)
+	);
+
+	$this->elements=array_combine($keys, $elements);
+      }
+    }
+
+    foreach($this->elements as $k=>$element) {
+      if(!isset($data[$k]))
+	$data[$k] = null;
+
+      $r = $element->set_request_data($data[$k]);
+
+      if($r === false)
+	$ret = false;
+    }
+
+    if(isset($remove)) {
+      foreach($remove as $k=>$dummy) {
+	unset($this->data[$k]);
+	unset($this->elements[$k]);
+      }
+      $this->changed_count=true;
+    }
+
+    unset($this->data);
+
+    return $ret;
+  }
+
+  function set_orig_data($data) {
+    if((!isset($data))||(!is_array($data)))
+      $data=array();
+    $this->orig_data=$data;
+
+    foreach($data as $k=>$v) {
+      if(isset($this->elements[$k])) {
+	$this->elements[$k]->set_orig_data($v);
+      }
+    }
+  }
+
+  function get_orig_data() {
+    return $this->orig_data;
+  }
+
+  function build_form() {
+    $this->elements=array();
+
+    $data = array();
+    if($this->def['default'] > 0)
+      $data=array_fill(0, $this->def['default'], null);
+
+    if(isset($this->data)&&is_array($this->data))
+      $data=$this->data;
+
+    $element_class=get_form_element_class($this->def['def']);
+    $element_def=$this->def['def'];
+    foreach($data as $k=>$v) {
+      $element_id="{$this->id}_{$k}";
+      $element_options=$this->options;
+      $element_options['var_name']="{$this->options['var_name']}[{$k}]";
+      $element_def['_name']="#".(sizeof($this->elements)+1);
+
+      $this->elements[$k]=new $element_class($element_id, $element_def, $element_options, $this);
+    }
+  }
+
+  function show_element_part($k, $element, $document) {
+    $order = ((!array_key_exists("order", $this->def)) || ($this->def['order'] == true)) ? "order": "no_order";
+    $removeable = ((!array_key_exists("removeable", $this->def)) || ($this->def['removeable'] !== false)) ? "removeable" : "not_removeable";
+
+    // wrapper #k
+    $div=$document->createElement("div");
+    $div->setAttribute("form_element_num", $k);
+    $div->setAttribute("class", "form_element_filters_part");
+
+    // element #k
+    $el_div=$document->createElement("span");
+    $el_div->setAttribute("form_element_num", $k);
+    $el_div->setAttribute("class", "form_element_filters_part_element form_element_{$element->type()} form_element_filters_{$order}_{$removeable}");
+    $div->appendChild($el_div);
+
+    $el_div->appendChild($element->show_element($document));
+
+    // errors #k
+    $el_div->appendChild($element->show_div_errors($document));
+
+    // Actions #k
+    $el_div=$document->createElement("span");
+    $el_div->setAttribute("form_element_num", $k);
+    $el_div->setAttribute("class", "form_element_filters_part_element_actions");
+    $div->appendChild($el_div);
+
+    if($order == "order") {
+      $input=$document->createElement("input");
+      $input->setAttribute("type", "submit");
+      $input->setAttribute("name", "{$this->options['var_name']}[__order_up][{$k}]");
+      $input->setAttribute("value", "↑");
+      $el_div->appendChild($input);
+
+      $input=$document->createElement("input");
+      $input->setAttribute("type", "submit");
+      $input->setAttribute("name", "{$this->options['var_name']}[__order_down][{$k}]");
+      $input->setAttribute("value", "↓");
+      $el_div->appendChild($input);
+    }
+
+    if ($removeable === 'removeable') {
+      $input=$document->createElement("input");
+      $input->setAttribute("type", "submit");
+      $input->setAttribute("name", "{$this->options['var_name']}[__remove][{$k}]");
+      $input->setAttribute("value", "X");
+      $el_div->appendChild($input);
+    }
+
+    return $div;
+  }
+
+  function show_element($document) {
+    $div=parent::show_element($document);
+
+    foreach($this->elements as $k=>$element) {
+      $part_div=$this->show_element_part($k, $element, $document);
+      $div->appendChild($part_div);
+    }
+
+    $el_div=$document->createElement("div");
+    $el_div->setAttribute("class", "form_element_filters_actions");
+    $div->appendChild($el_div);
+
+    $input=$document->createElement("input");
+    $input->setAttribute("type", "submit");
+    $input->setAttribute("name", "{$this->options['var_name']}[__new]");
+    if(array_key_exists("button:add_element", $this->def)) {
+      if(is_array($this->def['button:add_element']))
+        $input->setAttribute("value", lang($this->def['button:add_element']));
+      else
+        $input->setAttribute("value", $this->def['button:add_element']);
+    }
+    else
+      $input->setAttribute("value", lang("form:add_element"));
+    $el_div->appendChild($input);
+
+    if(isset($this->def['max']) && (sizeof($this->elements) >= $this->def['max'])) {
+      $input->setAttribute("class", "reached_max");
+    }
+
+    if(isset($this->def['min']) && (sizeof($this->elements) <= $this->def['min'])) {
+      $input->setAttribute("class", "reached_min");
+    }
+
+    return $div;
+  }
+
+  function save_data() {
+    parent::save_data();
+
+    foreach($this->elements as $k=>$element) {
+      $element->save_data();
+    }
+  }
+
+  function is_modified() {
+    $orig_data=$this->get_orig_data();
+
+    foreach($this->elements as $k=>$element) {
+      // new key, was not present in orig_data
+      if(!isset($orig_data[$k]))
+	return true;
+
+      // data of sub-elements were changed
+      if($element->is_modified())
+	return true;
+    }
+
+    foreach($orig_data as $k=>$v) {
+      // key has been removed
+      if(!isset($this->elements[$k]))
+	return true;
+    }
+
+    return false;
+  }
+
+  function check_required(&$errors, $param) {
+    if($this->required()) {
+      if(sizeof($this->elements))
+        return;
+
+      if(sizeof($param)<2)
+        $errors[]=lang('form:require_value');
+      else
+        $errors[]=$param[1];
+    }
+  }
+
+  function refresh($force=false) {
+    parent::refresh($force);
+
+    foreach($this->elements as $k=>$element) {
+      $element->refresh($force);
+    }
+  }
+}
