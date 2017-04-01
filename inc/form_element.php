d468cf487602dc404eb720d5a5ee3d4376c3a928
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-04-01 20:17:21 +0200

    Form Element.path_name(): all elements can be root element

--- inc/form_element.php
+++ inc/form_element.php
@@ -57,6 +57,9 @@ class form_element {
   }
 
   function path_name() {
+    if($this->form_parent===null)
+      return null;
+
     $parent_path=$this->form_parent->path_name();
 
     if($parent_path===null)
