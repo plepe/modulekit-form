75b64ce7c7e9d75ac6896f1789548ef37675714b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-04-02 20:42:06 +0200

    Form Element Filters: don't access non available elements

--- inc/form_element_filters.php
+++ inc/form_element_filters.php
@@ -69,7 +69,9 @@ class form_element_filters extends form_element {
           $this->add_element($k);
         }
 
-        $this->elements[$k]->set_data($d);
+        if (array_key_exists($k, $this->elements)) {
+          $this->elements[$k]->set_data($d);
+        }
       }
 
     foreach ($this->elements as $k => $element) {
@@ -217,7 +219,9 @@ class form_element_filters extends form_element {
     }
 
     foreach ($this->orig_data_elements as $k) {
-      $ret[$k] = $this->available_elements[$k]->get_orig_data();
+      if (array_key_exists($k, $this->available_elements)) {
+        $ret[$k] = $this->available_elements[$k]->get_orig_data();
+      }
     }
 
     return $ret;
@@ -250,7 +254,8 @@ class form_element_filters extends form_element {
     }
 
     if (!array_key_exists($k, $this->available_elements)) {
-      trigger_error("{$this->id} - add_element: \"{$k}\" not found", E_USER_ERROR);
+      trigger_error("{$this->id} - add_element: \"{$k}\" not found", E_USER_WARNING);
+      return;
     }
 
     $this->elements[$k] = $this->available_elements[$k];
